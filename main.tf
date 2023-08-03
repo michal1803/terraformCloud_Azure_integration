f712bfd04ffd31fb4a5ba3116047752cedb79130
michal1803 <michaw49@gmail.com>
2023-08-03 14:42:27 +0200

    Add connection to Azure and create RG

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 3.0.2"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
 
@@ -20,23 +20,23 @@ terraform {
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = "westus2"
 
   tags = {
     Enviroment = "Terraform Getting Started"
-    Team = "DevOps"
+    Team       = "DevOps"
   }
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name = "myTFVnet"
-  address_space = ["10.0.0.0/16"]
-  location = "northeurope"
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "northeurope"
   resource_group_name = azurerm_resource_group.rg.name
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "resource_group_id" {
-  value = azurerm_resource_group.rg.id 
+  value = azurerm_resource_group.rg.id
 }
