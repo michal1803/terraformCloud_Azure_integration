74f8eced546d3e9fa2e7fa38f257a1063f584c22
michal1803 <michaw49@gmail.com>
2023-08-07 16:07:37 +0200

    create nsg and ns rule

--- terraform_azure/main.tf
+++ terraform_azure/main.tf
@@ -36,7 +36,28 @@ resource "azurerm_subnet" "michal-subnet" {
   virtual_network_name = azurerm_virtual_network.michal-vnet.name
   address_prefixes     = ["10.10.10.0/24"]
 
+}
+
+resource "azurerm_network_security_group" "michal-nsg" {
+  name = "michal-nsg"
+  location = azurerm_resource_group.michal-rg.location
+  resource_group_name = azurerm_resource_group.michal-rg.name
+
   tags = {
     environment = "Terraform Azure"
   }
+}
+
+resource "azurerm_network_security_rule" "michal-ns-rule" {
+  name                        = "michal-ns-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.michal-rg.name
+  network_security_group_name = azurerm_network_security_group.michal-nsg.name
 }
