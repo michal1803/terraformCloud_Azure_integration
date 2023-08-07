dbbe67f2a21331fdd846716958092e5776126046
michal1803 <michaw49@gmail.com>
2023-08-07 15:34:52 +0200

    create subnet

--- terraform_azure/main.tf
+++ terraform_azure/main.tf
@@ -29,3 +29,14 @@ resource "azurerm_virtual_network" "michal-vnet" {
     environment = "Terraform Azure"
   }
 }
+
+resource "azurerm_subnet" "michal-subnet" {
+  name                 = "michal-subnet"
+  resource_group_name  = azurerm_resource_group.michal-rg.name
+  virtual_network_name = azurerm_virtual_network.michal-vnet.name
+  address_prefixes     = ["10.10.10.0/24"]
+
+  tags = {
+    environment = "Terraform Azure"
+  }
+}
